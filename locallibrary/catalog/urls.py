64ef557e5dad6812a543584a4cfd8002f0790eb2
Данил Жиряков <gr404_ghidis@lab-404-8.tomtit-tomsk.ru>
2022-09-29 09:48:32 +0700

    Initial commit

---
+++ locallibrary/catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+urlpatterns = [
+
+]
+
